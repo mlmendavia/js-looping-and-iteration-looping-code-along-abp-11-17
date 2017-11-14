63ec920093ebb5e35398d7bedf90ff7589bac978
Melanie Mendavia <mlmendavia@gmail.com>
2017-11-14 22:41:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+printBadges(['Ada', 'Brendan', 'Ali']);
+// LOG: Welcome Ada! You are employee #1.
+// LOG: Welcome Brendan! You are employee #2.
+// LOG: Welcome Ali! You are employee #3.
+// => ["Ada", "Brendan", "Ali"]
+console.log();
+
