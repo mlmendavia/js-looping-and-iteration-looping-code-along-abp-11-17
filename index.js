20bd0d903983b75c56ae4dd757b61b2968a5a6d8
Melanie Mendavia <mlmendavia@gmail.com>
2017-11-14 22:50:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-// Code your solutions in this file
-printBadges(['Ada', 'Brendan', 'Ali']);
-// LOG: Welcome Ada! You are employee #1.
-// LOG: Welcome Brendan! You are employee #2.
-// LOG: Welcome Ali! You are employee #3.
-// => ["Ada", "Brendan", "Ali"]
-console.log();
+// // Code your solutions in this file
+// printBadges(['Ada', 'Brendan', 'Ali']);
+// // LOG: Welcome Ada! You are employee #1.
+// // LOG: Welcome Brendan! You are employee #2.
+// // LOG: Welcome Ali! You are employee #3.
+// // => ["Ada", "Brendan", "Ali"]
+const badges =['Ada', 'Brendan', 'Ali'];
+
+function printBadges(badges){
+  console.log(printBadges)
+}
 
